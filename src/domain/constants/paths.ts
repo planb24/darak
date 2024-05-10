678bdf64fec6b282988af42f4d5d4e13a0b7a712
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2024-05-10 19:19:25 +0900

    feat: 이미지 asset 구축

---
+++ src/domain/constants/paths.ts
@@ -0,0 +1,7 @@
+export const LOGIN_PATH = "/login" as const;
+export const SIGNUP_PATH = "/signup" as const;
+export const BASE_PATH = "/" as const;
+export const EVENT_DETAIL_PATH = "/history/:eventId" as const;
+export const CHAT_PATH = "/chat/:eventId" as const;
+export const EVENT_CREATE_PATH = "/post" as const;
+export const PREF_PATH = "/myaccount" as const;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_KEY: string;
+    readonly VITE_AUTH_DOMAIN: string;
+    readonly VITE_PROJECT_ID: string;
+    readonly VITE_STORAGE_BUCKET: string;
+    readonly VITE_MESSAGING_SENDER_ID: string;
+    readonly VITE_APP_ID: string;
+    readonly VITE_BASE_URL: string;
+    readonly VITE_TOS_URL: string;
+    readonly VITE_BOOK_API_URL: string;
+    readonly VITE_BOOK_API_KEY: string;
+    readonly VITE_USER_AGENT: string;
+}
