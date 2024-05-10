7335fa257afe4789878f31c46a6550b783466c55
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2024-05-10 19:02:22 +0900

    feat: 의존성 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ColorModeScript } from '@chakra-ui/react';
+import { theme } from './theme/index';
+
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config?.initialColorMode} />
     <App />
   </React.StrictMode>,
-)
+);
